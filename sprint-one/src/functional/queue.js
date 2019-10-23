ca0b0d672df26c1a6ead5f7c3efafe869860c221
Desmond Rogers <desmondrogers2@gmail.com>
2019-10-23 15:57:54 -0700

    Implement methods for functional

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -5,14 +5,29 @@ var Queue = function() {
   var storage = {};
 
   // Implement the methods below
+  var count = 1;
 
   someInstance.enqueue = function(value) {
+    storage[count] = value;
+    count++;
   };
 
   someInstance.dequeue = function() {
+    var result;
+    for (var key in storage) {
+      result = storage[key];
+      delete storage[key];
+      break;
+    }
+    return result;
   };
 
   someInstance.size = function() {
+    var result = 0;
+    for (var key in storage) {
+      result++;
+    }
+    return result;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,23 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    var len = someInstance.size();
+    storage[len + 1] = value;
   };
 
   someInstance.pop = function() {
+    var len = someInstance.size();
+    var lastIn = storage[len];
+    delete storage[len];
+    return lastIn;
   };
 
   someInstance.size = function() {
+    var result = 0;
+    for (var key in storage) {
+      result++;
+    }
+    return result;
   };
 
   return someInstance;
