5202d63f70c6eed2aca6acd67c1cfac03bd596f0
Desmond Rogers <desmondrogers2@gmail.com>
2019-10-23 21:48:46 -0700

    Implement queue for pseudoclassical pattern

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -26,5 +26,3 @@ var stackMethods = {
     return result;
   }
 };
-
-
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,25 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
 };
 
+Stack.prototype.push = function(value) {
+  var len = this.size();
+  this.storage[len + 1] = value;
+};
 
+Stack.prototype.pop = function() {
+  var len = this.size();
+  var lastIn = this.storage[len];
+  delete this.storage[len];
+  return lastIn;
+};
+
+Stack.prototype.size = function() {
+  var result = 0;
+  for (var key in this.storage) {
+    result++;
+  }
+  return result;
+};
