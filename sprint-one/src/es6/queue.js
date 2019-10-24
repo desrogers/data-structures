a4ea6d869c6762a31d361d0c953affaba487c879
Desmond Rogers <desmondrogers2@gmail.com>
2019-10-23 21:59:24 -0700

    Implement queue and stack structures for es6 style instantiation

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,31 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.count = 0;
+  }
+
+  enqueue(value) {
+    this.storage[this.count] = value;
+    this.count++;
+  }
+
+  dequeue() {
+    var firstInQueue;
+    for (var key in this.storage) {
+      firstInQueue = this.storage[key];
+      delete this.storage[key];
+      break;
+    }
+    return firstInQueue;
+  }
+
+  size() {
+    var result = 0;
+    for (var key in this.storage) {
+      result++;
+    }
+    return result;
   }
 
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,27 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+  }
+
+  push(value) {
+    var len = this.size();
+    this.storage[len + 1] = value;
+  }
+
+  pop() {
+    var len = this.size();
+    var lastIn = this.storage[len];
+    delete this.storage[len];
+    return lastIn;
+  }
+
+  size() {
+    var result = 0;
+    for (var key in this.storage) {
+      result++;
+    }
+    return result;
   }
 
 }
